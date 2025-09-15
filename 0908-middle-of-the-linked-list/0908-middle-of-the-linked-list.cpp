36a05a339c5c6eda1ee64b9c81d8718208fa014a
Raunak Pandey <panditraunak.143@gmail.com>
2025-09-15 15:20:32 +0530

    Time: 0 ms (100%), Space: 10 MB (62.59%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -11,28 +11,39 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int n =0;
-        while(temp->next != NULL){
-            n++;
-            temp = temp->next;
-        }
 
-        int middle = n/2;
-        ListNode* trav= head;
-        if(n%2==0){
-            for(int i =0; i<middle; i++){
-                trav = trav->next;
-            }
+// one pass method
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        else{
-            for(int i =0; i<=middle; i++){
-                trav = trav->next;
-            }
-        }
-        // for(int i =0; i<=n/2; i++){
-        //         trav = trav->next;
+        return slow;
+
+        // ListNode* temp = head;
+        // int n =0;
+        // while(temp->next != NULL){
+        //     n++;
+        //     temp = temp->next;
         // }
-        return trav;
+
+        // int middle = n/2;
+        // ListNode* trav= head;
+        // if(n%2==0){
+        //     for(int i =0; i<middle; i++){
+        //         trav = trav->next;
+        //     }
+        // }
+        // else{
+        //     for(int i =0; i<=middle; i++){
+        //         trav = trav->next;
+        //     }
+        // }
+        // // for(int i =0; i<=n/2; i++){
+        // //         trav = trav->next;
+        // // }
+        // return trav;
     }
 };
