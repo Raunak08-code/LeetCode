469969fb22225172aace1b2a5a09186df049c93a
Raunak Pandey <panditraunak.143@gmail.com>
2025-09-18 17:06:54 +0530

    Time: 11 ms (83.22%), Space: 58.3 MB (59.69%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* merge(ListNode* a,ListNode* b){
+        if(a==NULL) return b;
+        if(b==NULL) return a;
+        if(a->val<=b->val){ a->next = merge(a->next,b) ; return a;}
+        else { b->next = merge(a,b->next); return b;}
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* a = head;
+        ListNode* b = slow->next;
+        slow->next = NULL;
+        a = sortList(a);
+        b = sortList(b);
+        ListNode* c = merge(a,b);
+        return c;
+
+        
+    }
+};
