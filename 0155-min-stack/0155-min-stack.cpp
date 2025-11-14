cb8cb9861251ebf10bcc518bc25f68d4b9ef3112
Raunak Pandey <panditraunak.143@gmail.com>
2025-11-14 23:43:14 +0530

    Time: 2 ms (67.84%), Space: 24.4 MB (9.52%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,16 +1,65 @@
+// class MinStack {
+// public:
+//     stack<int> st;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         stack<int> helper;
+//         int mini = INT_MAX;
+//         while(st.size()>0){
+//             int x = st.top();
+//             mini = min(mini,x);
+//             helper.push(x);
+//             st.pop();
+            
+//         }
+//         while(helper.size()>0){
+//             st.push(helper.top());
+//             helper.pop();
+//         }
+//         return mini;
+//     }
+// };
+
+
+
+// method 2 
+
 class MinStack {
 public:
     stack<int> st;
+    stack<int> helper;
     MinStack() {
         
     }
     
     void push(int val) {
         st.push(val);
+        if(helper.size() == 0 ||val<helper.top()){
+            helper.push(val);
+        }
+        else{
+            helper.push(helper.top());
+        }
     }
     
     void pop() {
         st.pop();
+        helper.pop();
     }
     
     int top() {
@@ -18,22 +67,11 @@ public:
     }
     
     int getMin() {
-        stack<int> helper;
-        int mini = INT_MAX;
-        while(st.size()>0){
-            int x = st.top();
-            mini = min(mini,x);
-            helper.push(x);
-            st.pop();
-            
-        }
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return mini;
+        return helper.top();
     }
 };
+
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
