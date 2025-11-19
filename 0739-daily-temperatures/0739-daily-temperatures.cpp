06f277bfed5280f9c3f2a0126e037215915d94e3
Raunak Pandey <panditraunak.143@gmail.com>
2025-11-19 22:49:15 +0530

    Time: 18 ms (75.44%), Space: 102.9 MB (71.05%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -7,12 +7,13 @@ public:
         // ngi[n-1] = 0;
 
         for(int i=n-1; i>=0; i--){
+            //pop
             while(st.size()>0 && temperatures[st.top()] <= temperatures[i]){
                 st.pop();
             }
-            
+            //ans
             if(st.size() != 0) ngi[i] = st.top()-i;
-
+            //push
             st.push(i);
         }
         return ngi;
