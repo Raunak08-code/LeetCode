098ad8c7dfcae61395caa7d71e21d97e8252b337
Raunak Pandey <panditraunak.143@gmail.com>
2026-02-10 13:54:54 +0530

    Time: 0 ms (100%), Space: 12.8 MB (46.27%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-         if(p == NULL && q == NULL) return true;
+        if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
         // if(p->val == q->val){
         //     isSameTree(p->left,q->left);
@@ -20,6 +20,14 @@ public:
         //     return true;
         // }
         // else return false;  
-        return p->val == q->val&&isSameTree(p->left,q->left)&&isSameTree(p->right, q->right);
+        // //return p->val == q->val&&isSameTree(p->left,q->left)&&isSameTree(p->right, q->right);
+
+        if(p->val != q->val) return false; 
+        bool lst = isSameTree(p->left,q->left);
+        if(lst == false) return false;
+        bool rst = isSameTree(p->right, q->right);
+        if(rst == false ) return false;
+        return true;
+
     }
 };
